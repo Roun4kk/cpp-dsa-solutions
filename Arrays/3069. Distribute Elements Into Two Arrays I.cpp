867ac5252dda24dc0c6f6250009a236eb5420179
Rounak <molpariyarounak@gmail.com>
2026-08-20 12:55:54 +0530

    Add solution for distributing elements into two arrays
    Implement a solution to distribute elements into two arrays based on their values.

---
+++ Arrays/3069. Distribute Elements Into Two Arrays I.cpp
@@ -0,0 +1,28 @@
+// 3069. Distribute Elements Into Two Arrays I
+// Topics - Array
+// Platform - Leetcode
+// Tc - O(n)
+// Sc - O(n)
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1 , arr2;
+        for(auto it : nums){
+            if(arr1.empty()){
+                arr1.push_back(it);
+            }else if (arr2.empty()){
+                arr2.push_back(it);
+            }else{
+                if(arr1.back() > arr2.back()){
+                    arr1.push_back(it);
+                }else{
+                    arr2.push_back(it);
+                }
+            }
+        }
+        for(auto it : arr2){
+            arr1.push_back(it);
+        }
+        return arr1;
+    }
+};
